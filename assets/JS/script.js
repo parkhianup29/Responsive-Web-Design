ee2b94abeba15cbecf80744da1123dd5dd884086
AnupParkhi <62320337+parkhianup29@users.noreply.github.com>
2023-03-07 12:53:46 +0530

    initial setup

---
+++ assets/JS/script.js
@@ -0,0 +1,8 @@
+function toggle() {
+  let id = document.getElementById('hidden');
+  if (id.style.display === 'none') {
+    id.style.display = 'block';
+  } else {
+    id.style.display = 'none';
+  }
+}
